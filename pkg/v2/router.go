d5837f83b8065b93dace650bde842be243d5db50
Ryan King <rking@cogolabs.com>
2019-05-05 13:54:31 -0400

    refactor: setup separate filters package

--- pkg/v2/router.go
+++ pkg/v2/router.go
@@ -1,30 +1,25 @@
 package v2
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/ryantking/marina/pkg/v2/routes"
+	"github.com/ryantking/marina/pkg/web/filters"
 
 	"github.com/emicklei/go-restful"
-	log "github.com/sirupsen/logrus"
 )
 
 // NewRouter creates a router for the v2 API with all routes
 func NewRouter() http.Handler {
 	container := restful.NewContainer()
 	container.Add(routes.Registry())
-	container.Filter(func(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
-		log.Debugf("%s %s", req.Request.Method, req.Request.URL)
-		fmt.Println(req.Request.Header)
-		chain.ProcessFilter(req, resp)
-	})
+	container.Filter(filters.RequestLogger)
+	container.Filter(filters.PanicRecovery)
+	container.Filter(container.OPTIONSFilter)
 
 	cors := restful.CrossOriginResourceSharing{
-		ExposeHeaders: []string{"Location", "Docker-Upload-UUID", "Content-Length", "Range"},
-		// ExposeHeaders: []string{},
+		ExposeHeaders:  []string{"Location", "Docker-Upload-UUID", "Content-Length", "Range"},
 		AllowedHeaders: []string{"Content-Type", "Content-Length", "Content-Range", "Accept"},
-		// AllowedHeaders: []string{},
 		AllowedMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"},
 		CookiesAllowed: false,
 		Container:      container,
---
+++ pkg/web/response/errors.go
@@ -0,0 +1,22 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/emicklei/go-restful"
+	log "github.com/sirupsen/logrus"
+)
+
+// WriteError writes an error to the response with the given HTTP status code
+func WriteError(resp *restful.Response, httpStatus int, err error) {
+	err = resp.WriteError(httpStatus, err)
+	if err != nil {
+		log.WithError(err).Error("error writing to response")
+	}
+}
+
+// SendServerError sends a server error response to the client
+func SendServerError(resp *restful.Response, err error, msg string) {
+	WriteError(resp, http.StatusInternalServerError, err)
+	log.WithError(err).Error(msg)
+}
