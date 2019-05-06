13495773aade3e5179812aef7e58d34aaacc0fbc
Ryan King <rking@cogolabs.com>
2019-05-06 10:05:10 -0400

    schema: add initial migration

---
+++ migrations/1_init.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE `upload`;
+DROP TABLE `layer`;
+DROP TABLE `tag`;
+DROP TABLE `repository`;
+DROP TABLE `organization`;
---
+++ migrations/1_init.up.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `organization` (
+	`name` VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE `repository` (
+	`name` VARCHAR(255) NOT NULL,
+	`org_name` VARCHAR(255) NOT NULL,
+	INDEX (`name`),
+	FOREIGN KEY (`org_name`) REFERENCES `organization` (`name`)
+);
+
+CREATE TABLE `tag` (
+	`name` VARCHAR(255) NOT NULL,
+	`repo_name` VARCHAR(255) NOT NULL,
+	`org_name` VARCHAR(255) NOT NULL,
+	`manifest` JSON NOT NULL,
+	FOREIGN KEY (`repo_name`) REFERENCES `repository` (`name`),
+	FOREIGN KEY (`org_name`) REFERENCES `organization` (`name`)
+);
+
+CREATE TABLE `layer` (
+	`digest` VARCHAR(1024) NOT NULL,
+	`repo_name` VARCHAR(255) NOT NULL,
+	`org_name` VARCHAR(255) NOT NULL,
+	FOREIGN KEY (`repo_name`) REFERENCES `repository` (`name`),
+	FOREIGN KEY (`org_name`) REFERENCES `organization` (`name`)
+);
+
+CREATE TABLE `upload` (
+	`uuid` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	`done` BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+INSERT INTO `organization` (`name`)
+VALUES ('library');
