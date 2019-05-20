2aba366a8b3cebfa9dc7eb48f2a1bbb3a3c280bf
Ryan King <hello@ryanking.com>
2019-05-20 02:06:51 -0400

    refactor: kind of get tag list working

--- pkg/v2/handlers/tag/handlers.go
+++ pkg/v2/handlers/tag/handlers.go
@@ -1,30 +1,89 @@
 package tag
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/ryantking/marina/pkg/prisma"
+	"github.com/ryantking/marina/pkg/web"
+)
+
 const (
 	headerLink = "Link"
 )
 
+var (
+	client  = prisma.New(nil)
+	orderBy = prisma.TagOrderByInputUpdatedAtDesc
+)
+
 // List lists all tags for a given repository
-// func List(c echo.Context) error {
-// 	repoName, orgName, err := parsePath(c)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	pageSize, last, err := docker.ParsePagination(c)
-// 	if err != nil {
-// 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-// 	}
-// 	tags, nextLast, err := tagList(repoName, orgName, pageSize, last)
-// 	if err != nil {
-// 		return errors.Wrap(err, "error getting list of tags")
-// 	}
-//
-// 	if nextLast != "" {
-// 		link := fmt.Sprintf("/v2/%s/%s/tags/list?n=%d&last=%s", orgName, repoName, pageSize, nextLast)
-// 		c.Response().Header().Set(headerLink, link)
-// 	}
-// 	res := map[string]interface{}{}
-// 	res["name"] = fmt.Sprintf("%s/%s", orgName, repoName)
-// 	res["tags"] = tags
-// 	return c.JSON(http.StatusOK, res)
-// }
+func List(c echo.Context) error {
+	repo, org, err := web.ParsePath(c)
+	if err != nil {
+		return err
+	}
+	n, last, err := web.ParsePagination(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if n == 0 {
+		return listAll(c, repo, org)
+	}
+
+	return listPaginated(c, repo, org, n, last)
+}
+
+func listAll(c echo.Context, repo, org string) error {
+	tags, err := client.Tags(&prisma.TagsParams{
+		OrderBy: &orderBy,
+		Where: &prisma.TagWhereInput{
+			Image: &prisma.ImageWhereInput{
+				Repo: &prisma.RepositoryWhereInput{
+					Name: &repo,
+					Org:  &prisma.OrganizationWhereInput{Name: &org},
+				},
+			},
+		},
+	}).Exec(c.Request().Context())
+	if err != nil {
+		return err
+	}
+
+	return writeTags(c, repo, org, tags)
+}
+
+func listPaginated(c echo.Context, repo, org string, n int32, last string) error {
+	tags, err := client.Tags(&prisma.TagsParams{
+		OrderBy: &orderBy,
+		After:   &last,
+		First:   &n,
+		Where: &prisma.TagWhereInput{
+			Image: &prisma.ImageWhereInput{
+				Repo: &prisma.RepositoryWhereInput{
+					Name: &repo,
+					Org:  &prisma.OrganizationWhereInput{Name: &org},
+				},
+			},
+		},
+	}).Exec(c.Request().Context())
+	if err != nil {
+		return err
+	}
+
+	link := fmt.Sprintf("/v2/%s/%s/tags/list?n=%d&last=%s", org, repo, n, tags[len(tags)-1].Ref)
+	c.Response().Header().Set(headerLink, link)
+	return writeTags(c, repo, org, tags)
+}
+
+func writeTags(c echo.Context, repo, org string, tags []prisma.Tag) error {
+	tagRefs := make([]string, len(tags))
+	for i, tag := range tags {
+		tagRefs[i] = tag.Ref
+	}
+	res := map[string]interface{}{}
+	res["name"] = fmt.Sprintf("%s/%s", org, repo)
+	res["tags"] = tagRefs
+	return c.JSON(http.StatusOK, res)
+}
--- pkg/v2/handlers/tag/tag_test.go
+++ pkg/v2/handlers/tag/tag_test.go
@@ -1,7 +1,9 @@
 package tag
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/labstack/echo"
@@ -19,82 +21,20 @@ func (suite *TagTestSuite) SetupSuite() {
 	suite.r = e
 }
 
-// func (suite *TagTestSuite) TestParsePath() {
-// 	assert := suite.Assert()
-// 	require := suite.Require()
-//
-// 	c := new(mocks.Context)
-// 	c.On("Param", "repo").Return("testRepo")
-// 	c.On("Param", "org").Return("testOrg")
-// 	repoExists = func(repoName, orgName string) (bool, error) {
-// 		return true, nil
-// 	}
-//
-// 	repoName, orgName, err := parsePath(c)
-// 	require.NoError(err)
-// 	assert.Equal("testRepo", repoName)
-// 	assert.Equal("testOrg", orgName)
-// 	c.AssertExpectations(suite.T())
-// }
-//
-// func (suite *TagTestSuite) TestParsePathNonExistentRepo() {
-// 	assert := suite.Assert()
-//
-// 	c := new(mocks.Context)
-// 	c.On("Param", "repo").Return("testRepo")
-// 	c.On("Param", "org").Return("testOrg")
-// 	c.On("Set", "docker_err_code", docker.CodeNameUnknown)
-// 	repoExists = func(repoName, orgName string) (bool, error) {
-// 		return false, nil
-// 	}
-//
-// 	_, _, err := parsePath(c)
-// 	assert.Equal(echo.NewHTTPError(http.StatusNotFound, "no such repository"), err)
-// 	c.AssertExpectations(suite.T())
-// }
-//
-// func (suite *TagTestSuite) TestParsePathExistsError() {
-// 	assert := suite.Assert()
-//
-// 	c := new(mocks.Context)
-// 	c.On("Param", "repo").Return("testRepo")
-// 	c.On("Param", "org").Return("testOrg")
-// 	repoExists = func(repoName, orgName string) (bool, error) {
-// 		return false, fmt.Errorf("test error")
-// 	}
-//
-// 	_, _, err := parsePath(c)
-// 	assert.EqualError(errors.Cause(err), "test error")
-// 	c.AssertExpectations(suite.T())
-// }
-//
-// func (suite *TagTestSuite) TestList() {
-// 	assert := suite.Assert()
-// 	require := suite.Require()
-//
-// 	repoExists = func(repoName, orgName string) (bool, error) {
-// 		assert.Equal("testRepo", repoName)
-// 		assert.Equal("testOrg", orgName)
-// 		return true, nil
-// 	}
-// 	tagList = func(repoName, orgName string, pageSize uint, last string) ([]string, string, error) {
-// 		assert.Equal("testRepo", repoName)
-// 		assert.Equal("testOrg", orgName)
-// 		assert.EqualValues(5, pageSize)
-// 		assert.Zero(last)
-// 		return []string{"tag1", "tag2", "tag3", "tag4", "tag5"}, "tag5", nil
-// 	}
-//
-// 	req := httptest.NewRequest(http.MethodGet, "/v2/testOrg/testRepo/tags/list?n=5", nil)
-// 	rr := httptest.NewRecorder()
-// 	suite.r.ServeHTTP(rr, req)
-// 	require.Equal(http.StatusOK, rr.Code)
-// 	b, err := ioutil.ReadAll(rr.Body)
-// 	require.NoError(err)
-// 	expected := `{"name": "testOrg/testRepo", "tags": ["tag1", "tag2", "tag3", "tag4", "tag5"]}`
-// 	assert.JSONEq(expected, string(b))
-// 	assert.Equal("/v2/testOrg/testRepo/tags/list?n=5&last=tag5", rr.Header().Get(headerLink))
-// }
+func (suite *TagTestSuite) TestList() {
+	assert := suite.Assert()
+	require := suite.Require()
+
+	req := httptest.NewRequest(http.MethodGet, "/v2/library/alpine/tags/list", nil)
+	rr := httptest.NewRecorder()
+	suite.r.ServeHTTP(rr, req)
+	require.Equal(http.StatusOK, rr.Code)
+	b, err := ioutil.ReadAll(rr.Body)
+	require.NoError(err)
+	expected := `{"name": "testOrg/testRepo", "tags": ["tag1", "tag2", "tag3", "tag4", "tag5"]}`
+	assert.JSONEq(expected, string(b))
+	assert.Equal("/v2/testOrg/testRepo/tags/list?n=5&last=tag5", rr.Header().Get(headerLink))
+}
 
 func TestBaseTestSuite(t *testing.T) {
 	tests := new(TagTestSuite)
